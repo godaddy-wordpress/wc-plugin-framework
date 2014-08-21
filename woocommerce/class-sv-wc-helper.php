eae708c761f8f6bc851f6af238795123898c3f1a
Max Rice <max@maxrice.com>
2014-08-20 23:22:27 -0400

    Add remaining helper functions, closes #40
    * str_*() methods are now multibyte-safe
    * added array_insert_after() method
    * added array_to_xml() method

--- woocommerce/class-sv-wc-helper.php
+++ woocommerce/class-sv-wc-helper.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'SV_WC_Helper' ) ) :
 	class SV_WC_Helper {
 
 
-		/** String manipulation functions *************************************/
+		/** String manipulation functions (all multi-byte safe) ***************/
 
 		/**
 		 * Returns true if the haystack string starts with needle
@@ -51,7 +51,18 @@ if ( ! class_exists( 'SV_WC_Helper' ) ) :
 		 */
 		public static function str_starts_with( $haystack, $needle) {
 
-			return '' === $needle || 0 === strpos( $haystack, $needle );
+			if ( '' === $needle ) {
+				return true;
+			}
+
+			if ( self::multibyte_loaded() ) {
+
+				return 0 === mb_strpos( $haystack, $needle );
+
+			} else {
+
+				return 0 === strpos( self::str_to_ascii( $haystack ), self::str_to_ascii( $needle ) );
+			}
 		}
 
 
@@ -60,13 +71,28 @@ if ( ! class_exists( 'SV_WC_Helper' ) ) :
 		 *
 		 * Note: case-sensitive
 		 *
+		 * @since 2.1-1
 		 * @param $haystack
 		 * @param $needle
 		 * @return bool
 		 */
 		public static function str_ends_with( $haystack, $needle ) {
 
-			return '' === $needle || substr( $haystack, -strlen( $needle ) ) === $needle;
+			if ( '' === $needle ) {
+				return true;
+			}
+
+			if ( self::multibyte_loaded() ) {
+
+				return mb_substr( $haystack, -mb_strlen( $needle ) ) === $needle;
+
+			} else {
+
+				$haystack = self::str_to_ascii( $haystack );
+				$needle   = self::str_to_ascii( $needle );
+
+				return substr( $haystack, -strlen( $needle ) ) === $needle;
+			}
 		}
 
 
@@ -82,9 +108,217 @@ if ( ! class_exists( 'SV_WC_Helper' ) ) :
 		 */
 		public static function str_contains( $haystack, $needle ) {
 
-			return false !== strpos( $haystack, $needle );
+			if ( self::multibyte_loaded() ) {
+
+				return false !== mb_strpos( $haystack, $needle );
+
+			} else {
+
+				return false !== strpos( self::str_to_ascii( $haystack ), self::str_to_ascii( $needle ) );
+			}
 		}
 
+
+		/**
+		 * Truncates a given $string after a given $length if string is longer than
+		 * $length. The last characters will be replaced with the $omission string
+		 * for a total length not exceeding $length
+		 *
+		 * @since 2.1-1
+		 * @param string $string text to truncate
+		 * @param int $length total desired length of string, including omission
+		 * @param string $omission omission text, defaults to '...'
+		 * @return string
+		 */
+		public static function str_truncate( $string, $length, $omission = '...' ) {
+
+
+			if ( self::multibyte_loaded() ) {
+
+				$length -= mb_strlen( $omission );
+
+				return mb_substr( $string, 0, $length ) . $omission;
+
+			} else {
+
+				$string = self::str_to_ascii( $string );
+
+				$length -= strlen( $omission );
+
+				return substr( $string, 0, $length ) . $omission;
+			}
+		}
+
+
+		/**
+		 * Returns a string with all non-ASCII characters removed. This is useful
+		 * for any string functions that expect only ASCII chars and can't
+		 * safely handle UTF-8
+		 *
+		 * @since 2.1-1
+		 * @param string $string string to make ASCII
+		 * @return string|null ASCII string or null if error occurred
+		 */
+		public static function str_to_ascii( $string ) {
+
+			return iconv('UTF-8', 'ASCII//IGNORE', $string);
+		}
+
+
+		/**
+		 * Helper method to check if the multibyte extension is loaded, which
+		 * indicates it's safe to use the mb_*() string methods
+		 *
+		 * @since 2.1-1
+		 * @return bool
+		 */
+		private static function multibyte_loaded() {
+
+			return extension_loaded( 'mbstring' );
+		}
+
+
+		/** Array functions ***************************************************/
+
+
+		/**
+		 * Insert the given element after the given key in the array
+		 *
+		 * Sample usage:
+		 *
+		 * given
+		 *
+		 * array( 'item_1' => 'foo', 'item_2' => 'bar' )
+		 *
+		 * array_insert_after( $array, 'item_1', array( 'item_1.5' => 'w00t' ) )
+		 *
+		 * becomes
+		 *
+		 * array( 'item_1' => 'foo', 'item_1.5' => 'w00t', 'item_2' => 'bar' )
+		 *
+		 * @since 2.1-1
+		 * @param array $array array to insert the given element into
+		 * @param string $insert_key key to insert given element after
+		 * @param array $element element to insert into array
+		 * @return array
+		 */
+		public static function array_insert_after( Array $array, $insert_key, Array $element ) {
+
+			$new_array = array();
+
+			foreach ( $array as $key => $value ) {
+
+				$new_array[ $key ] = $value;
+
+				if ( $insert_key == $key ) {
+
+					foreach ( $element as $k => $v ) {
+						$new_array[ $k ] = $v;
+					}
+				}
+			}
+
+			return $new_array;
+		}
+
+
+		/**
+		 * Convert array into XML by recursively generating child elements
+		 *
+		 * First instantiate a new XML writer object:
+		 *
+		 * $xml = new XMLWriter();
+		 *
+		 * Open in memory (alternatively you can use a local URI for file output)
+		 *
+		 * $xml->openMemory();
+		 *
+		 * Then start the document
+		 *
+		 * $xml->startDocument( '1.0', 'UTF-8' );
+		 *
+		 * Don't forget to end the document and output the memory
+		 *
+		 * $xml->endDocument();
+		 *
+		 * $your_xml_string = $xml->outputMemory();
+		 *
+		 * @since 2.1-1
+		 * @param \XMLWriter $xml_writer XML writer instance
+		 * @param string|array $element_key name for element, e.g. <per_page>
+		 * @param string|array $element_value value for element, e.g. 100
+		 * @return string generated XML
+		 */
+		public static function array_to_xml( $xml_writer, $element_key, $element_value = array() ) {
+
+			if ( is_array( $element_value ) ) {
+
+				// handle attributes
+				if ( '@attributes' === $element_key ) {
+					foreach ( $element_value as $attribute_key => $attribute_value ) {
+
+						$xml_writer->startAttribute( $attribute_key );
+						$xml_writer->text( $attribute_value );
+						$xml_writer->endAttribute();
+					}
+					return;
+				}
+
+				// handle multi-elements (e.g. multiple <Order> elements)
+				if ( is_numeric( key( $element_value ) ) ) {
+
+					// recursively generate child elements
+					foreach ( $element_value as $child_element_key => $child_element_value ) {
+
+						$xml_writer->startElement( $element_key );
+
+						foreach ( $child_element_value as $sibling_element_key => $sibling_element_value ) {
+							self::array_to_xml( $xml_writer, $sibling_element_key, $sibling_element_value );
+						}
+
+						$xml_writer->endElement();
+					}
+
+				} else {
+
+					// start root element
+					$xml_writer->startElement( $element_key );
+
+					// recursively generate child elements
+					foreach ( $element_value as $child_element_key => $child_element_value ) {
+						self::array_to_xml( $xml_writer, $child_element_key, $child_element_value );
+					}
+
+					// end root element
+					$xml_writer->endElement();
+				}
+
+			} else {
+
+				// handle single elements
+				if ( '@value' == $element_key ) {
+
+					$xml_writer->text( $element_value );
+
+				} else {
+
+					// wrap element in CDATA tags if it contains illegal characters
+					if ( false !== strpos( $element_value, '<' ) || false !== strpos( $element_value, '>' ) ) {
+
+						$xml_writer->startElement( $element_key );
+						$xml_writer->writeCdata( $element_value );
+						$xml_writer->endElement();
+
+					} else {
+
+						$xml_writer->writeElement( $element_key, $element_value );
+					}
+
+				}
+
+				return;
+			}
+		}
 
 	}
 
