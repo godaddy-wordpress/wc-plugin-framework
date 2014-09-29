d9bd449c0abdce767bbdf7bfa310b98450397764
Tamara Zuk <tamarazuk@gmail.com>
2014-09-29 17:12:48 -0400

    3.0.0-1 Versioning

--- woocommerce/class-sv-wc-plugin.php
+++ woocommerce/class-sv-wc-plugin.php
@@ -84,12 +84,12 @@ if ( ! class_exists( 'SV_WC_Plugin' ) ) :
  * Use the standard WordPress/WooCommerce `is_*` methods when adding the notice
  * to control which pages it does (or does not) display on.
  *
- * @version 3.0.0
+ * @version 3.0.0-1
  */
 abstract class SV_WC_Plugin {
 
 	/** Plugin Framework Version */
-	const VERSION = '3.0.0';
+	const VERSION = '3.0.0-1';
 
 	/** @var string plugin id */
 	private $id;
