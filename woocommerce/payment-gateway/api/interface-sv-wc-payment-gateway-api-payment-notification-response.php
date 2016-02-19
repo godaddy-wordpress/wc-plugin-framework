facad5985b0d258c488313d7dd3bc8d9d6521941
Chase Wiseman <contact@chasewiseman.com>
2016-02-19 09:44:02 -0800

    [#103] Move response payment type method
    closes #103

--- woocommerce/payment-gateway/api/interface-sv-wc-payment-gateway-api-payment-notification-response.php
+++ woocommerce/payment-gateway/api/interface-sv-wc-payment-gateway-api-payment-notification-response.php
@@ -66,16 +66,6 @@ interface SV_WC_Payment_Gateway_API_Payment_Notification_Response extends SV_WC_
 
 
 	/**
-	 * Returns the payment type: 'credit-card', 'echeck', etc
-	 *
-	 * TODO: move to SV_WC_Payment_Gateway_API_Response
-	 * @since 2.2.0
-	 * @return string payment type or null if not available
-	 */
-	public function get_payment_type();
-
-
-	/**
 	 * Returns the card PAN or checking account number, if available
 	 *
 	 * @since 2.2.0
