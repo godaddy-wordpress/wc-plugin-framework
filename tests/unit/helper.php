e415eed38dd3b64758b9f1774b1b9b9d0c9ea769
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2016-02-23 18:01:59 -0500

    [#131] Add test methods in helper test class
    covers:
    
    - str_to_ascii
    - str_to_sane_utf8
    - array_to_xml
    - number_format
    - get_request (compared with Chase)
    
    adds few phpdocs too, and a Nyan Cat printer :)

--- tests/unit/helper.php
+++ tests/unit/helper.php
@@ -14,6 +14,10 @@ class Helper extends Test_Case {
 
 
 	/**
+	 * Test \SV_WC_Helper::str_starts_with()
+	 *
+	 * @see \SV_WC_Helper::str_starts_with()
+	 * @since 4.3.0-dev
 	 * @dataProvider provider_test_str_starts_with_true
 	 */
 	public function test_str_starts_with_true( $haystack, $needle ) {
@@ -21,8 +25,12 @@ class Helper extends Test_Case {
 		$this->assertTrue( \SV_WC_Helper::str_starts_with( $haystack, $needle ) );
 	}
 
+
 	/**
+	 * Test \SV_WC_Helper::str_starts_with()
 	 *
+	 * @see \SV_WC_Helper::str_starts_with()
+	 * @since 4.3.0-dev
 	 * @dataProvider provider_test_str_starts_with_false
 	 */
 	public function test_str_starts_with_false( $haystack, $needle ) {
@@ -34,6 +42,7 @@ class Helper extends Test_Case {
 	/**
 	 * Test SV_WC_Helper::wc_notice_count()
 	 *
+	 * @see \SV_WC_Helper::wc_notice_count()
 	 * @since 4.3.0-dev
 	 */
 	public function test_wc_notice_count() {
@@ -59,6 +68,7 @@ class Helper extends Test_Case {
 	/**
 	 * Test SV_WC_Helper::wc_add_notice()
 	 *
+	 * @see \SV_WC_Helper::wc_add_notice()
 	 * @since 4.3.0-dev
 	 */
 	public function test_wc_add_notice() {
@@ -80,6 +90,7 @@ class Helper extends Test_Case {
 	/**
 	 * Test SV_WC_Helper::wc_print_notice()
 	 *
+	 * @see \SV_WC_Helper::wc_print_notice()
 	 * @since 4.3.0-dev
 	 */
 	public function test_wc_print_notice() {
@@ -101,6 +112,7 @@ class Helper extends Test_Case {
 	/**
 	 * Test SV_WC_Helper::get_wc_log_file_url()
 	 *
+	 * @see SV_WC_Helper::get_wc_log_file_url()
 	 * @since 4.3.0-dev
 	 */
 	public function test_get_wc_log_file_url() {
@@ -126,6 +138,12 @@ class Helper extends Test_Case {
 	}
 
 
+	/**
+	 * Get an array with matched initial substrings
+	 *
+	 * @since 4.3.0-dev
+	 * @return array
+	 */
 	public function provider_test_str_starts_with_true() {
 
 		return [
@@ -135,6 +153,13 @@ class Helper extends Test_Case {
 		];
 	}
 
+
+	/**
+	 * Get an array with wrong initial substrings
+	 *
+	 * @since 4.3.0-dev
+	 * @return array
+	 */
 	public function provider_test_str_starts_with_false() {
 
 		return [
@@ -145,7 +170,175 @@ class Helper extends Test_Case {
 
 
 	/**
-	 * Test \SV_WC_Helper::get_post
+	 * Test \SV_WC_Helper::str_to_ascii()
+	 *
+	 * @see \SV_WC_Helper::str_to_ascii()
+	 * @since 4.3.0-dev
+	 * @dataProvider provider_test_str_to_ascii
+	 */
+	public function test_str_to_ascii( $string, $ascii ) {
+
+		$this->assertEquals( \SV_WC_Helper::str_to_ascii( $string ), $ascii );
+	}
+
+
+	/**
+	 * Get ASCII text strings
+	 *
+	 * @since 4.3.0-dev
+	 * @return array
+	 */
+	public function provider_test_str_to_ascii() {
+
+		return [
+			[ 'a\bc`1/2*3', 'a\bc`1/2*3' ],
+			[ 'question mark�', 'question mark' ],
+			[ 'poker♠1♣2♥3♦abc', 'poker123abc' ],
+			[ 'one half ½', 'one half ' ], // note the whitespace on the right
+			[ '10¢', '10' ] // that's not a c, that's ¢ as in cent, on some fonts this might not be obvious
+		];
+	}
+
+
+	/**
+	 * Test \SV_WC_Helper::str_to_sane_utf8()
+	 *
+	 * @see \SV_WC_Helper::str_to_sane_utf8()
+	 * @since 4.3.0-dev
+	 * @dataProvider provider_test_str_to_sane_utf8
+	 */
+	public function test_str_to_sane_utf8( $string, $utf8 ) {
+
+		$this->assertEquals( \SV_WC_Helper::str_to_sane_utf8( $string ), $utf8 );
+	}
+
+
+	/**
+	 * Get UTF8 text strings
+	 *
+	 * @since 4.3.0-dev
+	 * @return array
+	 */
+	public function provider_test_str_to_sane_utf8() {
+
+		return [
+			[ 'a\bc`1/2*3', 'a\bc1/2*3' ],
+			[ 'question mark�', 'question mark' ],
+			[ 'poker♠1♣2♥3♦abc', 'poker123abc' ],
+			[ 'one half ½', 'one half ' ], // note the whitespace on the right
+			[ '10¢', '10¢' ] // that's not a c, that's ¢ as in cent, on some fonts this might not be obvious
+		];
+	}
+
+
+	/**
+	 * Test \SV_WC_Helper::array_to_xml()
+	 *
+	 * @see \SV_WC_Helper::array_to_xml()
+	 * @since 4.3.0-dev
+	 */
+	public function test_array_to_xml() {
+
+		$xml = new \XMLWriter();
+		$xml->openMemory();
+		$xml->startDocument( '1.0', 'UTF-8' );
+
+		\SV_WC_Helper::array_to_xml( $xml, 'foo', array( 'bar' => 'baz' ) );
+
+		$xml->endDocument();
+
+		$output   = $xml->outputMemory();
+
+		// Mind newlines, empty last line and indentation
+		$expected = <<<MSG
+<?xml version="1.0" encoding="UTF-8"?>
+<foo><bar>baz</bar></foo>
+
+MSG;
+		$this->assertEquals( $output, $expected );
+	}
+
+
+	/**
+	 * Test \SV_WC_Helper::number_format()
+	 *
+	 * @see \SV_WC_Helper::number_format()
+	 * @since 4.3.0-dev
+	 * @dataProvider provider_test_number_format
+	 */
+	public function test_number_format( $original_number, $formatted_number ) {
+
+		$this->assertTrue( is_numeric( \SV_WC_Helper::number_format( $original_number ) ), true  );
+		$this->assertEquals( \SV_WC_Helper::number_format( $original_number ), $formatted_number );
+	}
+
+
+	/**
+	 * Get an array with formatted numbers
+	 *
+	 * @since 4.3.0-dev
+	 * @return array
+	 */
+	public function provider_test_number_format() {
+
+		return [
+			[ '1', '1.00' ],
+			[ '10', '10.00' ],
+		    [ '1000', '1000.00' ],
+			[ '1.23', '1.23' ],
+			[ '10.20', '10.20' ],
+			[ '10.201', '10.20' ],
+			[ '1000.90', '1000.90'],
+		];
+	}
+
+
+	/**
+	 * Test \SV_WC_Helper::get_request()
+	 *
+	 * @see \SV_WC_Helper::get_request()
+	 * @since 4.3.0-dev
+	 * @dataProvider provider_test_get_request_associative_array
+	 */
+	public function test_get_request( $request_key, $request_value ) {
+
+		$_REQUEST[ $request_key ]  = $request_value;
+
+		$this->assertEquals( \SV_WC_Helper::get_request( $request_key ), trim( $request_value ) );
+		$this->assertEquals( \SV_WC_Helper::get_request( 'invalidKey' ), '' );
+	}
+
+
+	/**
+	 * Get an associative array with different keys and values
+	 *
+	 * @since 4.3.0-dev
+	 * @return array
+	 */
+	public function provider_test_get_request_associative_array() {
+
+		return [ [
+			'empty'       , '',
+			'integer'     , 123,
+			'float'       , 1.23,
+			'string'      , 'abc',
+			'whitespace'  , ' abc ',
+			'true'        , true,
+			'false'       , false,
+			'array'       , [ 'a', 'b', 'c' ],
+			'array_assoc' , [ 'a' => 'x', 'b' => 'y', 'c' => 'z' ],
+			'CaMeLKey'    , 'CaMeLvalue',
+			'UCKey'       , 'UCValue',
+			'lckey'       , 'lcvalue',
+		] ];
+	}
+
+
+	/**
+	 * Test \SV_WC_Helper::get_post()
+	 *
+	 * @see \SV_WC_Helper::get_post()
+	 * @since 4.3.0-dev
 	 */
 	public function test_get_post() {
 
@@ -167,30 +360,9 @@ class Helper extends Test_Case {
 
 
 	/**
-	 * Test \SV_WC_Helper::test_get_request
-	 */
-	public function test_get_request() {
-
-		$key = 'sv_test_key';
-
-		// Test for an unset key
-		$this->assertEquals( '', \SV_WC_Helper::get_request( $key ) );
-
-		$_REQUEST[ $key ] = 'value';
-
-		// Check that a value is returned
-		$this->assertEquals( 'value', \SV_WC_Helper::get_request( $key ) );
-
-		$_REQUEST[ $key ] = ' untrimmed-value ';
-
-		// Check that the value is trimmed
-		$this->assertEquals( 'untrimmed-value', \SV_WC_Helper::get_request( $key ) );
-	}
-
-
-	/**
 	 * Test \SV_WC_Helper::array_insert_after
 	 *
+	 * @see \SV_WC_Helper::array_insert_after()
 	 * @since 4.3.0-dev
 	 */
 	public function test_array_insert_after() {
@@ -214,4 +386,6 @@ class Helper extends Test_Case {
 
 		$this->assertEquals( $target_array, \SV_WC_Helper::array_insert_after( $target_array, $insert_point, $added_array ) );
 	}
+
+
 }
