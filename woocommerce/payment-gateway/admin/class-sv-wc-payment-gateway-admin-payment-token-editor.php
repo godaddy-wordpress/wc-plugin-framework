af846c22ef1089240c1bae62fe97ae36e257eeef
Max Rice <max@maxrice.com>
2016-03-31 18:47:21 -0400

    [#123] add discover to admin token editor card type select
    see code comment

--- woocommerce/payment-gateway/admin/class-sv-wc-payment-gateway-admin-payment-token-editor.php
+++ woocommerce/payment-gateway/admin/class-sv-wc-payment-gateway-admin-payment-token-editor.php
@@ -556,6 +556,11 @@ class SV_WC_Payment_Gateway_Admin_Payment_Token_Editor {
 		$card_types = $this->get_gateway()->get_card_types();
 		$options    = array();
 
+		// TODO: the gateway card types setting for Discover (`DISC`) doesn't match what most gateway
+		// implementations will use as the token card type (`discover`), so manually add
+		// it for now, until we improve our consistency here @MR 2016-03-31
+		$card_types[] = 'discover';
+
 		foreach ( $card_types as $card_type ) {
 			$options[ strtolower( $card_type ) ] = SV_WC_Payment_Gateway_Helper::payment_type_to_name( $card_type );
 		}
