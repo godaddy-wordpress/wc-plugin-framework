2a9ab576fb329040c29697a157b050a5cb191b17
Chase Wiseman <chase@skyverge.com>
2017-07-18 13:22:35 -0700

    v5: add a sample plugin loader class for PHP 5.2

---
+++ woocommerce/woocommerce-framework-plugin-loader-sample.php
@@ -0,0 +1,270 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Framework Plugin TODO: plugin name
+ * Plugin URI: http://www.woocommerce.com/products/ TODO: product URL
+ * Description: TODO: plugin description
+ * Author: SkyVerge
+ * Author URI: http://www.woocommerce.com
+ * Version: 1.0.0 TODO: plugin version
+ * Text Domain: TODO: plugin textdomain
+ * Domain Path: /i18n/languages/
+ *
+ * Copyright: (c) 2011-2017, SkyVerge, Inc. (info@skyverge.com)
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package   TODO: package
+ * @author    SkyVerge
+ * @copyright Copyright (c) 2011-2017, SkyVerge, Inc.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ *
+ * Woo: 99999:00000000000000000000000000000000 TODO: updater keys
+ */
+
+defined( 'ABSPATH' ) or exit;
+
+// Required functions
+if ( ! function_exists( 'woothemes_queue_update' ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'woo-includes/woo-functions.php' );
+}
+
+// Plugin updates
+woothemes_queue_update( plugin_basename( __FILE__ ), '00000000000000000000000000000000', '99999' ); // TODO: updater keys
+
+// WC active check
+if ( ! is_woocommerce_active() ) {
+	return;
+}
+
+/**
+ * The plugin loader class.
+ *
+ * TODO: Rename the class, and replace all instances of SV_WC_Framework_Plugin_Loader
+ * TODO: Update all version numbers and @since tags to the plugin version
+ *
+ * @since 1.0.0
+ */
+class SV_WC_Framework_Plugin_Loader {
+
+
+	/** minimum PHP version required by this plugin */
+	const MINIMUM_PHP_VERSION = '5.3.0';
+
+	const PLUGIN_NAME = 'WooCommerce Framework Plugin'; // TODO: plugin name
+
+	/** @var SV_WC_Plugin_Loader single instance of this class */
+	protected static $instance;
+
+	/** @var array the admin notices to add */
+	protected $notices = array();
+
+
+	/**
+	 * Constructs the class.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function __construct() {
+
+		register_activation_hook( __FILE__, array( $this, 'activation_check' ) );
+
+		add_action( 'admin_init', array( $this, 'check_environment' ) );
+
+		add_action( 'admin_notices', array( $this, 'admin_notices' ), 15 );
+
+		// if the environment check fails, initialize the plugin
+		if ( $this->is_environment_compatible() ) {
+			add_action( 'plugins_loaded', array( $this, 'init_plugin' ) );
+		}
+	}
+
+
+	/**
+	 * Cloning instances is forbidden due to singleton pattern.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __clone() {
+		_doing_it_wrong( __FUNCTION__, sprintf( 'You cannot clone instances of %s.', get_class( $this ) ), '1.0.0' );
+	}
+
+
+	/**
+	 * Unserializing instances is forbidden due to singleton pattern.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __wakeup() {
+		_doing_it_wrong( __FUNCTION__, sprintf( 'You cannot unserialize instances of %s.', get_class( $this ) ), '1.0.0' );
+	}
+
+
+	/**
+	 * Initializes the plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	public function init_plugin() {
+
+		$this->load_framework();
+
+		// load the main plugin class
+		require_once( plugin_dir_path( __FILE__ ) . 'class-wc-framework-plugin.php' ); // TODO: main plugin class file
+
+		// fire it up!
+		wc_framework_plugin(); // TODO: call the main plugin method
+
+		do_action( 'sv_wc_framework_plugins_loaded' ); // TODO: we should eliminate the need for this {CW 2017-07-18}
+	}
+
+
+	/**
+	 * Loads the base framework classes.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function load_framework() {
+
+		if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_0_0\\SV_WC_Plugin' ) ) {
+			require_once( plugin_dir_path( __FILE__ ) . 'lib/skyverge/woocommerce/class-sv-wc-plugin.php' );
+		}
+
+		// TODO: remove this if not a payment gateway
+		if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_0_0\\SV_WC_Payment_Gateway_Plugin' ) ) {
+			require_once( plugin_dir_path( __FILE__ ) . 'lib/skyverge/woocommerce/payment-gateway/class-sv-wc-payment-gateway-plugin.php' );
+		}
+	}
+
+
+	/**
+	 * Checks the server environment and other factors and deactivates plugins as necessary.
+	 *
+	 * Based on http://wptavern.com/how-to-prevent-wordpress-plugins-from-activating-on-sites-with-incompatible-hosting-environments
+	 *
+	 * @since 1.0.0
+	 */
+	public function activation_check() {
+
+		if ( ! $this->is_environment_compatible() ) {
+
+			$this->deactivate_plugin();
+
+			wp_die( self::PLUGIN_NAME . ' could not be activated. ' . $this->get_environment_message() );
+		}
+	}
+
+	/**
+	 * Checks the environment on loading WordPress, just in case the environment changes after activation.
+	 *
+	 * @since 1.0.0
+	 */
+	public function check_environment() {
+
+		if ( ! $this->is_environment_compatible() && is_plugin_active( plugin_basename( __FILE__ ) ) ) {
+
+			$this->deactivate_plugin();
+
+			$this->add_admin_notice( 'bad_environment', 'error', self::PLUGIN_NAME . ' has been deactivated. ' . $this->get_environment_message() );
+		}
+	}
+
+
+	/**
+	 * Deactivates the plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	protected function deactivate_plugin() {
+
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+	}
+
+
+	/**
+	 * Adds an admin notice to be displayed.
+	 *
+	 * @since 1.0.0
+	 */
+	public function add_admin_notice( $slug, $class, $message ) {
+
+		$this->notices[ $slug ] = array(
+			'class'   => $class,
+			'message' => $message
+		);
+	}
+
+
+	/**
+	 * Displays any admin notices added with \SV_WC_Framework_Plugin_Loader::add_admin_notice()
+	 *
+	 * @since 1.0.0
+	 */
+	public function admin_notices() {
+
+		foreach ( (array) $this->notices as $notice_key => $notice ) {
+
+			echo "<div class='" . esc_attr( $notice['class'] ) . "'><p>";
+				echo wp_kses( $notice['message'], array( 'a' => array( 'href' => array() ) ) );
+			echo "</p></div>";
+		}
+	}
+
+
+	/**
+	 * Determines if the server environment is compatible with this plugin.
+	 *
+	 * Override this method to add checks for more than just the PHP version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
+	protected function is_environment_compatible() {
+
+		return version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '>=' );
+	}
+
+
+	/**
+	 * Gets the message for display when the environment is incompatible with this plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	protected function get_environment_message() {
+
+		$message = sprintf( 'The minimum PHP version required for this plugin is %1$s. You are running %2$s.', self::MINIMUM_PHP_VERSION, PHP_VERSION );
+
+		return $message;
+	}
+
+
+	/**
+	 * Gets the main \SV_WC_Framework_Plugin_Loader instance.
+	 *
+	 * Ensures only one instance can be loaded.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return \SV_WC_Framework_Plugin_Loader
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+
+}
+
+// fire it up!
+SV_WC_Framework_Plugin_Loader::instance();
