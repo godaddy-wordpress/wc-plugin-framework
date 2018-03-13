5ef0d8ac97553ea497f7b899c15f5b3a4ec257a3
Chase Wiseman <chase@skyverge.com>
2018-03-13 10:04:26 -0700

    Loader: add a version constant & getter for loading the framework files

--- woocommerce/woocommerce-framework-plugin-loader-sample.php
+++ woocommerce/woocommerce-framework-plugin-loader-sample.php
@@ -57,6 +57,9 @@ class SV_WC_Framework_Plugin_Loader {
 	/** minimum WooCommerce version required by this plugin */
 	const MINIMUM_WC_VERSION = '2.6';
 
+	/** SkyVerge plugin framework version used by this plugin */
+	const FRAMEWORK_VERSION = '5.1.0';
+
 	/** the plugin name, for displaying notices */
 	const PLUGIN_NAME = 'WooCommerce Framework Plugin'; // TODO: plugin name
 
@@ -137,14 +140,40 @@ class SV_WC_Framework_Plugin_Loader {
 	 */
 	protected function load_framework() {
 
-		if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_1_0\\SV_WC_Plugin' ) ) {
+		if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\' . $this->get_framework_version_namespace() . '\\SV_WC_Plugin' ) ) {
 			require_once( plugin_dir_path( __FILE__ ) . 'lib/skyverge/woocommerce/class-sv-wc-plugin.php' );
 		}
 
 		// TODO: remove this if not a payment gateway
-		if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_1_0\\SV_WC_Payment_Gateway_Plugin' ) ) {
+		if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\' . $this->get_framework_version_namespace() . '\\SV_WC_Payment_Gateway_Plugin' ) ) {
 			require_once( plugin_dir_path( __FILE__ ) . 'lib/skyverge/woocommerce/payment-gateway/class-sv-wc-payment-gateway-plugin.php' );
 		}
+	}
+
+
+	/**
+	 * Gets the framework version in namespace form.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	protected function get_framework_version_namespace() {
+
+		return 'v' . str_replace( '.', '_', $this->get_framework_version() );
+	}
+
+
+	/**
+	 * Gets the famework version used by this plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	protected function get_framework_version() {
+
+		return self::FRAMEWORK_VERSION;
 	}
 
 
