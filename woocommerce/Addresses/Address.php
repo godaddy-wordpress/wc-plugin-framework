8306c6e01a36a68110655b8efced0bc913aa6fbe
Chase Wiseman <chase@skyverge.com>
2018-09-06 16:23:47 -0700

    [#307] Introduce address objects
    w/ tests

---
+++ woocommerce/Addresses/Address.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * WooCommerce Plugin Framework
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@skyverge.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the plugin to newer
+ * versions in the future. If you wish to customize the plugin for your
+ * needs please refer to http://www.skyverge.com
+ *
+ * @package   SkyVerge/WooCommerce/Plugin/Classes
+ * @author    SkyVerge
+ * @copyright Copyright (c) 2013-2018, SkyVerge, Inc.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+namespace SkyVerge\WooCommerce\PluginFramework\v5_2_2\Addresses;
+
+defined( 'ABSPATH' ) or exit;
+
+if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_2_2\\Addresses\\Address' ) ) :
+
+class Address {
+
+
+	/** @var string line 1 of the street address */
+	protected $line_1 = '';
+
+	/** @var string line 2 of the street address */
+	protected $line_2 = '';
+
+	/** @var string line 3 of the street address */
+	protected $line_3 = '';
+
+	/** @var string address locality (city) */
+	protected $locality = '';
+
+	/** @var string address region (state) */
+	protected $region = '';
+
+	/** @var string address country */
+	protected $country = '';
+
+	/** @var string address postcode */
+	protected $postcode = '';
+
+
+	/** Getter methods ************************************************************************************************/
+
+
+	/**
+	 * Gets line 1 of the street address.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_line_1() {
+
+		return $this->line_1;
+	}
+
+
+	/**
+	 * Gets line 2 of the street address.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_line_2() {
+
+		return $this->line_2;
+	}
+
+
+	/**
+	 * Gets line 3 of the street address.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_line_3() {
+
+		return $this->line_3;
+	}
+
+
+	/**
+	 * Gets the locality or city.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_locality() {
+
+		return $this->locality;
+	}
+
+
+	/**
+	 * Gets the region or state.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_region() {
+
+		return $this->region;
+	}
+
+
+	/**
+	 * Gets the country.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_country() {
+
+		return $this->country;
+	}
+
+
+	/**
+	 * Gets the postcode.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_postcode() {
+
+		return $this->postcode;
+	}
+
+
+	/** Setter methods ************************************************************************************************/
+
+
+	/**
+	 * Sets line 1 of the street address.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value line 1 value
+	 */
+	public function set_line_1( $value ) {
+
+		$this->line_1 = $value;
+	}
+
+
+	/**
+	 * Sets line 2 of the street address.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value line 2 value
+	 */
+	public function set_line_2( $value ) {
+
+		$this->line_2 = $value;
+	}
+
+
+	/**
+	 * Gets line 3 of the street address.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value line 3 value
+	 */
+	public function set_line_3( $value ) {
+
+		$this->line_3 = $value;
+	}
+
+
+	/**
+	 * Gets the locality or city.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value locality value
+	 */
+	public function set_locality( $value ) {
+
+		$this->locality = $value;
+	}
+
+
+	/**
+	 * Gets the region or state.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value region value
+	 */
+	public function set_region( $value ) {
+
+		$this->region = $value;
+	}
+
+
+	/**
+	 * Sets the country.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value country value
+	 */
+	public function set_country( $value ) {
+
+		$this->country = $value;
+	}
+
+
+	/**
+	 * Sets the postcode.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value postcode value
+	 */
+	public function set_postcode( $value ) {
+
+		$this->postcode = $value;
+	}
+
+
+}
+
+endif;
---
+++ woocommerce/Addresses/Customer_Address.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * WooCommerce Plugin Framework
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@skyverge.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the plugin to newer
+ * versions in the future. If you wish to customize the plugin for your
+ * needs please refer to http://www.skyverge.com
+ *
+ * @package   SkyVerge/WooCommerce/Plugin/Classes
+ * @author    SkyVerge
+ * @copyright Copyright (c) 2013-2018, SkyVerge, Inc.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+namespace SkyVerge\WooCommerce\PluginFramework\v5_2_2\Addresses;
+use SkyVerge\WooCommerce\PluginFramework\v5_2_2 as Framework;
+
+defined( 'ABSPATH' ) or exit;
+
+if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_2_2\\Addresses\\Customer_Address' ) ) :
+
+class Customer_Address extends Address {
+
+
+	/** @var string customer first name */
+	protected $first_name = '';
+
+	/** @var string customer last name */
+	protected $last_name = '';
+
+
+	/** Getter Methods ************************************************************************************************/
+
+
+	/**
+	 * Gets the customer first name.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_first_name() {
+
+		return $this->first_name;
+	}
+
+
+	/**
+	 * Gets the customer first name.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @return string
+	 */
+	public function get_last_name() {
+
+		return $this->last_name;
+	}
+
+
+	/** Setter Methods ************************************************************************************************/
+
+
+	/**
+	 * Sets the customer first name.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value first name value
+	 */
+	public function set_first_name( $value ) {
+
+		$this->first_name = $value;
+	}
+
+
+	/**
+	 * Sets the customer last name.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param string $value first name value
+	 */
+	public function set_last_name( $value ) {
+
+		$this->last_name = $value;
+	}
+
+
+	/**
+	 * Sets the full address based on a WooCommerce order.
+	 *
+	 * @since 5.3.0-dev
+	 *
+	 * @param \WC_Order $order
+	 * @param string $type address type, like billing or shipping
+	 */
+	public function set_from_order( \WC_Order $order, $type = 'billing' ) {
+
+		$this->set_first_name( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_first_name" ) );
+		$this->set_last_name( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_last_name" ) );
+		$this->set_line_1( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_address_1" ) );
+		$this->set_line_2( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_address_2" ) );
+		$this->set_locality( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_city" ) );
+		$this->set_region( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_state" ) );
+		$this->set_country( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_country" ) );
+		$this->set_postcode( Framework\SV_WC_Order_Compatibility::get_prop( $order, "{$type}_postcode" ) );
+	}
+
+
+}
+
+endif;
