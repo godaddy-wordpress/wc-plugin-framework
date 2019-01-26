7b9f1f3d73063a65c2c3c25862ad9aaa44818d8f
Chase Wiseman <chase@skyverge.com>
2019-01-25 17:19:12 -0800

    Introduce the abstract & hosted payment handlers
    Moves the code away from specific gateway classes into a handler that
    any gateway can load and use.

--- woocommerce/payment-gateway/class-sv-wc-payment-gateway-plugin.php
+++ woocommerce/payment-gateway/class-sv-wc-payment-gateway-plugin.php
@@ -287,6 +287,8 @@ abstract class SV_WC_Payment_Gateway_Plugin extends SV_WC_Plugin {
 		require_once( $payment_gateway_framework_path . '/class-sv-wc-payment-gateway-my-payment-methods.php' );
 
 		// handlers
+		require_once( $payment_gateway_framework_path . '/Handlers/Abstract_Payment_Handler.php' );
+		require_once( $payment_gateway_framework_path . '/Handlers/Abstract_Hosted_Payment_Handler.php' );
 		require_once( $payment_gateway_framework_path . '/Handlers/Capture.php' );
 
 		// apple pay
