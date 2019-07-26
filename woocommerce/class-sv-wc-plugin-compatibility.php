4bb49893bda8ce665696d404d18f7b7054b0840c
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2019-07-26 16:09:45 +0800

    ch12277 Add helper method to connect a page to WC admin

--- woocommerce/class-sv-wc-plugin-compatibility.php
+++ woocommerce/class-sv-wc-plugin-compatibility.php
@@ -273,6 +273,23 @@ class SV_WC_Plugin_Compatibility {
 	}
 
 
+	/**
+	 * Connects a page to WooCommerce Admin, if available.
+	 *
+	 * @since 5.4.1-dev.1
+	 *
+	 * @see \WC_Admin_Page_Controller::connect_page() for $options
+	 *
+	 * @param array $options associative array of arguments
+	 */
+	public static function _wc_admin_page( $options ) {
+
+		if ( self::is_wc_admin_available() ) {
+			\WC_Admin_Page_Controller::get_instance()->connect_page( $options );
+		}
+	}
+
+
 	/** WordPress core ******************************************************/
 
 
