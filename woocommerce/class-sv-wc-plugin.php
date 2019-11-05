59df291480f24193151ac2afddb16c10de1c0379
Willington Vega <wvega@wvega.com>
2019-11-05 11:16:36 -0500

    Begin v5.5.1 [branch ch16760]

--- woocommerce/class-sv-wc-plugin.php
+++ woocommerce/class-sv-wc-plugin.php
@@ -43,7 +43,7 @@ abstract class SV_WC_Plugin {
 
 
 	/** Plugin Framework Version */
-	const VERSION = '5.5.0';
+	const VERSION = '5.5.1';
 
 	/** @var object single instance of plugin */
 	protected static $instance;
