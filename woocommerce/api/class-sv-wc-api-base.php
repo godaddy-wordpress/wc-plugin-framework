dcc997f551996bc896ce818078ee6fc418d7d959
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2019-12-24 10:56:04 +0800

    Deprecate SV_WC_API_Base::require_tls_1_2()

--- woocommerce/api/class-sv-wc-api-base.php
+++ woocommerce/api/class-sv-wc-api-base.php
@@ -797,10 +797,13 @@ abstract class SV_WC_API_Base {
 	 * Determines if TLS v1.2 is required for API requests.
 	 *
 	 * @since 4.4.0
+	 * @deprecated 5.5.2-dev.1
 	 *
 	 * @return bool
 	 */
 	public function require_tls_1_2() {
+
+		wc_deprecated_function( __METHOD__, '5.5.2', 'SV_WC_Plugin::require_tls_1_2()' );
 
 		return $this->get_plugin()->require_tls_1_2();
 	}
