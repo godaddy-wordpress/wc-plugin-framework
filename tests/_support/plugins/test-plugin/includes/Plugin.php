fc33727c564cff310639be3159aff4b8d8f3063a
Deiva Magalhaes <deiva@skyverge.com>
2020-01-15 17:16:20 -0800

    Dev versioning [branch ch8169]

--- tests/_support/plugins/test-plugin/includes/Plugin.php
+++ tests/_support/plugins/test-plugin/includes/Plugin.php
@@ -1,7 +1,7 @@
 <?php
 namespace SkyVerge\WooCommerce\TestPlugin;
 
-use SkyVerge\WooCommerce\PluginFramework\v5_5_3 as Framework;
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4 as Framework;
 
 defined( 'ABSPATH' ) or exit;
 
