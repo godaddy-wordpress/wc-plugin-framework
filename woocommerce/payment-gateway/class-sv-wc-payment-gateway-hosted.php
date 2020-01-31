8b714127fd79a468be072f728323121c1ecc1046
Chase Wiseman <chase@skyverge.com>
2020-01-31 10:51:15 -0800

    Revert "5.4.4 dev versioning"
    This reverts commit 3026246fe6104360086e80cab0b3f84bf02186cc.

--- woocommerce/payment-gateway/class-sv-wc-payment-gateway-hosted.php
+++ woocommerce/payment-gateway/class-sv-wc-payment-gateway-hosted.php
@@ -22,11 +22,11 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  */
 
-namespace SkyVerge\WooCommerce\PluginFramework\v5_4_4;
+namespace SkyVerge\WooCommerce\PluginFramework\v5_4_3;
 
 defined( 'ABSPATH' ) or exit;
 
-if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_4_4\\SV_WC_Payment_Gateway_Hosted' ) ) :
+if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_4_3\\SV_WC_Payment_Gateway_Hosted' ) ) :
 
 /**
  * # WooCommerce Payment Gateway Framework Hosted Gateway
