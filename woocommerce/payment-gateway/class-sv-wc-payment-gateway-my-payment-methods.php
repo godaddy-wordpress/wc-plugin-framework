81307f444419993101277b6f4e1be98bb7367dcb
Willington Vega <wvega@wvega.com>
2020-02-20 15:35:40 -0500

    Remove handler for woocommerce_after_account_payment_methods
    We are extending WooCommerce's payment methods table now, so there
    is no need to render the entire table anymore.

--- woocommerce/payment-gateway/class-sv-wc-payment-gateway-my-payment-methods.php
+++ woocommerce/payment-gateway/class-sv-wc-payment-gateway-my-payment-methods.php
@@ -103,9 +103,7 @@ class SV_WC_Payment_Gateway_My_Payment_Methods {
 		add_action( 'woocommerce_account_payment_methods_column_details', [ $this, 'add_payment_method_details' ] );
 		add_action( 'woocommerce_account_payment_methods_column_default', [ $this, 'add_payment_method_default' ] );
 
-		// render the My Payment Methods section
-		// TODO: merge our payment methods data into the core table and remove this in a future version {CW 2016-05-17}
-		add_action( 'woocommerce_after_account_payment_methods', array( $this, 'render' ) );
+		// render JavaScript used in the My Payment Methods section
 		add_action( 'woocommerce_after_account_payment_methods', array( $this, 'render_js' ) );
 
 		// handle payment method deletion, etc.
