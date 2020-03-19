a92c16950fbafc10713276f27c6b3918a4621f0a
Deiva Magalhaes <deiva@skyverge.com>
2020-03-19 15:10:27 -0700

    Unit test for set_id

--- tests/unit/Settings_API/SettingTest.php
+++ tests/unit/Settings_API/SettingTest.php
@@ -29,4 +29,37 @@ class SettingTest extends \Codeception\Test\Unit {
 	/** Test methods **************************************************************************************************/
 
 
+	/**
+	 * Tests \SkyVerge\WooCommerce\PluginFramework\v5_6_1\Settings_API\Setting::set_id()
+	 *
+	 * @param string $input input ID
+	 * @param string $expected expected return ID
+	 *
+	 * @dataProvider provider_set_id
+	 */
+	public function test_set_id( $input, $expected ) {
+
+		$setting = new Setting();
+		$setting->set_id( $input );
+		$this->assertEquals( $expected, $setting->get_id() );
+	}
+
+
+	/** Provider methods **********************************************************************************************/
+
+
+	/**
+	 * Provider for test_set_id()
+	 *
+	 * @return array
+	 */
+	public function provider_set_id() {
+
+		return [
+			[ 'my-setting', 'my-setting' ],
+			[ '', '' ],
+		];
+	}
+
+
 }
