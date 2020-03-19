54c6d79c0d011b79ba5e42faad88f6bb3375cc38
Deiva Magalhaes <deiva@skyverge.com>
2020-03-19 15:20:12 -0700

    Unit test for set_type

--- tests/unit/Settings_API/SettingTest.php
+++ tests/unit/Settings_API/SettingTest.php
@@ -45,6 +45,22 @@ class SettingTest extends \Codeception\Test\Unit {
 	}
 
 
+	/**
+	 * Tests \SkyVerge\WooCommerce\PluginFramework\v5_6_1\Settings_API\Setting::set_type()
+	 *
+	 * @param string $input input type
+	 * @param string $expected expected return type
+	 *
+	 * @dataProvider provider_set_type
+	 */
+	public function test_set_type( $input, $expected ) {
+
+		$setting = new Setting();
+		$setting->set_type( $input );
+		$this->assertEquals( $expected, $setting->get_type() );
+	}
+
+
 	/** Provider methods **********************************************************************************************/
 
 
@@ -58,6 +74,24 @@ class SettingTest extends \Codeception\Test\Unit {
 		return [
 			[ 'my-setting', 'my-setting' ],
 			[ '', '' ],
+		];
+	}
+
+
+	/**
+	 * Provider for test_set_type()
+	 *
+	 * @return array
+	 */
+	public function provider_set_type() {
+
+		return [
+			[ 'string', 'string' ],
+			[ 'url', 'url' ],
+			[ 'email', 'email' ],
+			[ 'integer', 'integer' ],
+			[ 'float', 'float' ],
+			[ 'boolean', 'boolean' ],
 		];
 	}
 
