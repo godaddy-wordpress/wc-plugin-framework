d8d97bb5c18406e3139d3fd0286801306a8e2a38
Willington Vega <wvega@wvega.com>
2020-03-19 20:39:51 -0500

    Add empty integration test class for Setting

---
+++ tests/integration/Settings_API/SettingTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use SkyVerge\WooCommerce\PluginFramework\v5_6_1\Settings_API\Setting;
+
+class SettingTest extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	protected function _before() {
+
+	}
+
+
+	protected function _after() {
+
+	}
+
+
+	/** Tests *********************************************************************************************************/
+
+
+}
