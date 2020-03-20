f5a48f7dae6c57eaa682f8ce5318cdc1e5f2da9a
Willington Vega <wvega@wvega.com>
2020-03-20 12:43:15 -0500

    Add test for get_setting() and unregister_setting()

--- tests/integration/Settings_API/AbstractSettingsTest.php
+++ tests/integration/Settings_API/AbstractSettingsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use SkyVerge\WooCommerce\PluginFramework\v5_6_1\Settings_API\Abstract_Settings;
+use SkyVerge\WooCommerce\PluginFramework\v5_6_1\Settings_API\Setting;
 
 /**
  * Tests for the Abstract_Settings class.
@@ -21,6 +22,7 @@ class AbstractSettingsTest extends \Codeception\TestCase\WPTestCase {
 	protected function _before() {
 
 		require_once 'woocommerce/Settings_API/Abstract_Settings.php';
+		require_once 'woocommerce/Settings_API/Setting.php';
 	}
 
 
@@ -36,6 +38,20 @@ class AbstractSettingsTest extends \Codeception\TestCase\WPTestCase {
 	public function test_constructor() {
 
 		$this->assertEquals( 'test-plugin', $this->get_settings()->id );
+	}
+
+
+	/**
+	 * @see Abstract_Settings::unregister_setting()
+	 * @see Abstract_Settings::get_setting()
+	 */
+	public function test_unregister_setting() {
+
+		$this->assertInstanceOf( Setting::class, $this->get_settings()->get_setting( 'test-setting' ) );
+
+		$this->get_settings()->unregister_setting( 'test-setting' );
+
+		$this->assertNull( $this->get_settings()->get_setting( 'test-setting' ) );
 	}
 
 
@@ -56,6 +72,8 @@ class AbstractSettingsTest extends \Codeception\TestCase\WPTestCase {
 
 				protected function register_settings() {
 
+					// TODO: remove when register_setting() is available and a setting object can be set in the test {WV 2020-03-20}
+					$this->settings['test-setting'] = new Setting();
 				}
 
 
