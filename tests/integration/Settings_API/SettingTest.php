bfa5aa0f45bd8769c9c5f234644671b244ae71c2
Deiva Magalhaes <deiva@skyverge.com>
2020-03-23 14:50:50 -0700

    Add integration test for Setting::update_value

--- tests/integration/Settings_API/SettingTest.php
+++ tests/integration/Settings_API/SettingTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use SkyVerge\WooCommerce\PluginFramework\v5_6_1\Settings_API\Setting;
+use SkyVerge\WooCommerce\PluginFramework\v5_6_1\SV_WC_Plugin_Exception;
 
 class SettingTest extends \Codeception\TestCase\WPTestCase {
 
@@ -61,14 +62,88 @@ class SettingTest extends \Codeception\TestCase\WPTestCase {
 
 
 	/**
+	 * @see Setting::update_value()
+	 *
+	 * @param mixed $value value to pass to method
+	 * @param string $type setting type
+	 * @param array $options setting options
+	 * @param mixed $expected setting value after execution
+	 * @param bool $exception whether an exception is expected
+	 *
+	 * @dataProvider provider_update_value
+	 */
+	public function test_update_value( $value, $type, $options, $expected, $exception ) {
+
+		if ( $exception ) {
+			$this->expectException( SV_WC_Plugin_Exception::class );
+		}
+
+		$setting = new Setting();
+		$setting->set_type( $type );
+		$setting->set_options( $options );
+
+		$setting->update_value( $value );
+
+		$this->assertSame( $expected, $setting->get_value() );
+	}
+
+
+	/**
+	 * Provider for test_update_value()
+	 *
+	 * @return array
+	 */
+	public function provider_update_value() {
+
+		require_once( 'woocommerce/Settings_API/Setting.php' );
+
+		return [
+			[ 'valid', Setting::TYPE_STRING, [], 'valid', false ],
+			[ 123, Setting::TYPE_STRING, [], null, true ],
+			[ 'green', Setting::TYPE_STRING, [ 'green', 'red' ], 'green', false ],
+			[ 'not an option', Setting::TYPE_STRING, [ 'green', 'red' ], null, true ],
+
+			[ 'https://skyverge.com/', Setting::TYPE_URL, [], 'https://skyverge.com/', false ],
+			[ 'file:///tmp/', Setting::TYPE_URL, [], null, true ],
+			[ 'https://skyverge.com/', Setting::TYPE_URL, [ 'https://skyverge.com/', 'http://skyverge.com/' ], 'https://skyverge.com/', false ],
+			[ 'https://google.com/', Setting::TYPE_URL, [ 'https://skyverge.com/', 'http://skyverge.com/' ], null, true ],
+
+			[ 'test@example.com', Setting::TYPE_EMAIL, [], 'test@example.com', false ],
+			[ 'not-an-email.com', Setting::TYPE_EMAIL, [], null, true ],
+			[ 'test@example.com', Setting::TYPE_EMAIL, [ 'test@example.com' ], 'test@example.com', false ],
+			[ 'another@example.com', Setting::TYPE_EMAIL, [ 'test@example.com' ], null, true ],
+
+			[ 12345, Setting::TYPE_INTEGER, [], 12345, false ],
+			[ 1.345, Setting::TYPE_INTEGER, [], null, true ],
+			[ '234', Setting::TYPE_INTEGER, [], null, true ],
+			[ 1, Setting::TYPE_INTEGER, [ 1, 2 ], 1, false ],
+			[ 3, Setting::TYPE_INTEGER, [ 1, 2 ], null, true ],
+
+			[ 12345, Setting::TYPE_FLOAT, [], 12345, false ],
+			[ 1.345, Setting::TYPE_FLOAT, [], 1.345, false ],
+			[ '234', Setting::TYPE_FLOAT, [], null, true ],
+			[ 1.5, Setting::TYPE_FLOAT, [ 1.5, 2.5 ], 1.5, false ],
+			[ 3.5, Setting::TYPE_FLOAT, [ 1.5, 2.5 ], null, true ],
+
+			[ true, Setting::TYPE_BOOLEAN, [], true, false ],
+			[ 'yes', Setting::TYPE_BOOLEAN, [], null, true ],
+			[ 1, Setting::TYPE_BOOLEAN, [], null, true ],
+			// it beats me why someone would have a boolean setting with only one option, but in theory it is possible
+			[ true, Setting::TYPE_BOOLEAN, [ true ], true, false ],
+			[ false, Setting::TYPE_BOOLEAN, [ true ], null, true ],
+		];
+	}
+
+
+	/**
 	 * @see Setting::validate_value()
 	 *
 	 * @param mixed $value value to pass to method
 	 * @param string $type setting type
-	 * @param bool $type whether the value should be considered valid or not
+	 * @param bool $expected whether the value should be considered valid or not
 	 *
 	 * @dataProvider provider_validate_value
-	 * */
+	 */
 	public function test_validate_value( $value, $type, $expected ) {
 
 		$setting = new Setting();
