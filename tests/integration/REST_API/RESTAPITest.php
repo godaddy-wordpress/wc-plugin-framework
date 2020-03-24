bbbb5a16beaba7d65e71218533c50dd8d483e2a6
Willington Vega <wvega@wvega.com>
2020-03-23 20:00:27 -0500

    Add empty integration test or REST_API class

---
+++ tests/integration/REST_API/RESTAPITest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Tests for the REST_API class.
+ *
+ * @see \SkyVerge\WooCommerce\PluginFramework\v5_6_1\REST_API
+ */
+class RESTAPITest extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+	protected function _before() {
+
+	}
+
+
+	protected function _after() {
+
+	}
+
+
+	/** Tests *********************************************************************************************************/
+
+
+	/** Helper methods ************************************************************************************************/
+
+
+}
