90b2c00506e3cdcf0048370418cc3a36b43ab152
Deiva Magalhaes <deiva@skyverge.com>
2020-03-24 13:16:07 -0700

    Return item (prepare_item_for_response already calls rest_ensure_response)

--- woocommerce/rest-api/Controllers/Settings.php
+++ woocommerce/rest-api/Controllers/Settings.php
@@ -139,6 +139,7 @@ class Settings extends \WP_REST_Controller {
 
 		if ( $setting = $this->settings->get_setting( $setting_id ) ) {
 
+			return $this->prepare_item_for_response( $setting );
 
 		} else {
 
