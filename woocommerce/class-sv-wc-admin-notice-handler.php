772c35a23c28a22df9f072087c3616cda44d20d5
Willington Vega <wvega@wvega.com>
2020-06-22 20:29:23 -0500

    Use array shorthand notation

--- woocommerce/class-sv-wc-admin-notice-handler.php
+++ woocommerce/class-sv-wc-admin-notice-handler.php
@@ -63,11 +63,11 @@ class SV_WC_Admin_Notice_Handler {
 
 		$this->plugin      = $plugin;
 
-		// render any admin notices, delayed notices, and
-		add_action( 'admin_notices', array( $this, 'render_admin_notices'         ), 15 );
+		// render any admin notices, delayed notices, and admin notice JS
+		add_action( 'admin_notices', [ $this, 'render_admin_notices'         ],              15 );
 		add_action( 'admin_footer',  [ $this, 'render_disable_admin_notices_conflict_fix' ], 10 );
-		add_action( 'admin_footer',  array( $this, 'render_delayed_admin_notices' ), 15 );
-		add_action( 'admin_footer',  array( $this, 'render_admin_notice_js'       ), 20 );
+		add_action( 'admin_footer',  [ $this, 'render_delayed_admin_notices' ],              15 );
+		add_action( 'admin_footer',  [ $this, 'render_admin_notice_js'       ],              20 );
 
 		// AJAX handler to dismiss any warning/error notices
 		add_action( 'wp_ajax_wc_plugin_framework_' . $this->get_plugin()->get_id() . '_dismiss_notice', array( $this, 'handle_dismiss_notice' ) );
