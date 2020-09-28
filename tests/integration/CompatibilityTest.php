f6f0cec14e00dc2ce387510cbc948b698badb94d
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-09-28 11:59:33 +0800

    Add integration test for is_rest_api_request

---
+++ tests/integration/CompatibilityTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use SkyVerge\WooCommerce\PluginFramework\v5_8_1 as Framework;
+
+/**
+ * Tests for the compatibility class.
+ *
+ * @see \SkyVerge\WooCommerce\PluginFramework\v5_8_1\SV_WC_Plugin_Compatibility
+ */
+class CompatibilityTest extends \Codeception\TestCase\WPTestCase {
+
+
+	public function test_is_rest_api_request() {
+		global $_SERVER;
+
+		$is_api_request = Framework\SV_WC_Plugin_Compatibility::is_rest_api_request();
+
+		$this->assertIsBool( $is_api_request );
+		$this->assertFalse( $is_api_request );
+	}
+
+
+}
