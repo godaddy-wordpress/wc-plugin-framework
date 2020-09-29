9c00fa95d61488958ed788426b964d3a9579c2c9
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-09-29 11:35:14 +0800

    Rewrite test so that the server global is properly set

--- tests/integration/HelperTest.php
+++ tests/integration/HelperTest.php
@@ -10,12 +10,35 @@ use SkyVerge\WooCommerce\PluginFramework\v5_8_1 as Framework;
 class HelperTest extends \Codeception\TestCase\WPTestCase {
 
 
-	public function test_is_rest_api_request() {
+	/**
+	 * Tests {@see Framework\SV_WC_Helper::is_rest_api_request()}.
+	 *
+	 * @param string $route endpoint
+	 * @param bool $expected result
+	 *
+	 * @dataProvider provider_is_rest_api_request
+	 */
+	public function test_is_rest_api_request( $route, $expected ) {
+
+		$_SERVER['REQUEST_URI'] = $route;
 
 		$is_api_request = Framework\SV_WC_Helper::is_rest_api_request();
 
-		$this->assertIsBool( $is_api_request );
-		$this->assertFalse( $is_api_request );
+		$this->assertSame( $is_api_request, $expected );
+	}
+
+
+	/**
+	 * Data provider for {@see HelperTest::test_is_rest_api_request()}.
+	 *
+	 * @return array[]
+	 */
+	public function provider_is_rest_api_request() {
+
+		return [
+			[ '/wp-json/', true ],
+			[ '/', false ],
+		];
 	}
 
 
