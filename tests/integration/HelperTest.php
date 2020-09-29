ab48237767ffd1352d92911018c7ccb8e7fa3984
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-09-29 11:28:21 +0800

    Rename tester class

--- tests/integration/HelperTest.php
+++ tests/integration/HelperTest.php
@@ -3,11 +3,11 @@
 use SkyVerge\WooCommerce\PluginFramework\v5_8_1 as Framework;
 
 /**
- * Tests for the compatibility class.
+ * Tests for the helper class.
  *
  * @see \SkyVerge\WooCommerce\PluginFramework\v5_8_1\SV_WC_Plugin_Compatibility
  */
-class CompatibilityTest extends \Codeception\TestCase\WPTestCase {
+class HelperTest extends \Codeception\TestCase\WPTestCase {
 
 
 	public function test_is_rest_api_request() {
