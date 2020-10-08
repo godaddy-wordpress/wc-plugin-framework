48b1665f45eb703de03c164e7533272f61e4507d
Deiva Magalhaes <deiva@skyverge.com>
2020-10-08 12:29:25 -0700

    Update Grunt uglify task to minify ES6 JS files

--- grunt/configs/uglify.js
+++ grunt/configs/uglify.js
@@ -3,9 +3,9 @@ module.exports = function( grunt ) {
 	'use strict';
 	var config = {};
 
-	// Add Uglify task with legacy rules
 	config.uglify = {
-		uglify: {
+		// Add Uglify task with legacy rules
+		fromCoffee: {
 			options : {
 				sourceMap: true,
 				sourceMapIncludeSources: true,
@@ -15,6 +15,21 @@ module.exports = function( grunt ) {
 			files : [{
 				src: [ '<%= dirs.gateway.js %>/js/frontend/sv-wc-payment-gateway-frontend.min.js' ], // uglify JS from CoffeeScript compilation
 				dest: '<%= dirs.gateway.js %>/frontend/sv-wc-payment-gateway-frontend.min.js'
+			}],
+		},
+		fromJs: {
+			options: {
+				sourceMap: true,
+				compress: true
+			},
+			files: [{
+				expand: true,
+				src: [ '<%= dirs.gateway.js %>/frontend/*.js', '!<%= dirs.gateway.js %>/frontend/*.min.js', '!<%= dirs.gateway.js %>/js/frontend/*.min.js' ], // uglify non-minified ES6 JS
+				dest: '<%= dirs.gateway.js %>/frontend',
+				cwd: '.',
+				rename: function (dst, src) {
+					return src.replace('.js', '.min.js');
+				}
 			}]
 		}
 	}
