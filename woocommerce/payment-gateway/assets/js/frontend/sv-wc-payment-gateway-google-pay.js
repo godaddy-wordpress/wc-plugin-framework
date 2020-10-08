b9993961b0255a7efc08e0c939e8a7c6602895fe
Deiva Magalhaes <deiva@skyverge.com>
2020-10-08 13:52:31 -0700

    Add SV_WC_Google_Pay_Handler_v5_8_1 JS class

--- woocommerce/payment-gateway/assets/js/frontend/sv-wc-payment-gateway-google-pay.js
+++ woocommerce/payment-gateway/assets/js/frontend/sv-wc-payment-gateway-google-pay.js
@@ -2,4 +2,11 @@ jQuery( document ).ready( ( $ ) => {
 
 	'use strict';
 
+	class SV_WC_Google_Pay_Handler_v5_8_1 {
+
+
+	}
+
+	$( document.body ).trigger( 'sv_wc_google_pay_handler_v5_8_1_loaded' );
+
 } );
