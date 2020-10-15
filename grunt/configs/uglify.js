1521388126b565b3e4d425d5c1ee5b5c04fe2a30
Deiva Magalhaes <deiva@skyverge.com>
2020-10-15 10:49:13 -0700

    Revert uglify task config changes

--- grunt/configs/uglify.js
+++ grunt/configs/uglify.js
@@ -3,35 +3,18 @@ module.exports = function( grunt ) {
 	'use strict';
 	var config = {};
 
+	// Add Uglify task with legacy rules
 	config.uglify = {
-		// Add Uglify task with legacy rules
-		fromCoffee: {
-			options : {
-				sourceMap: true,
-				sourceMapIncludeSources: true,
-				sourceMapIn: '<%= dirs.gateway.js %>/frontend/sv-wc-payment-gateway-frontend.min.js.map', // input sourcemap from CoffeeScript compilation
-				sourceMapName: '<%= dirs.gateway.js %>/frontend/sv-wc-payment-gateway-frontend.min.map'
-			},
-			files : [{
-				src: [ '<%= dirs.gateway.js %>/js/frontend/sv-wc-payment-gateway-frontend.min.js' ], // uglify JS from CoffeeScript compilation
-				dest: '<%= dirs.gateway.js %>/frontend/sv-wc-payment-gateway-frontend.min.js'
-			}],
+		options : {
+			sourceMap: true,
+			sourceMapIncludeSources: true,
+			sourceMapIn: '<%= dirs.gateway.js %>/frontend/sv-wc-payment-gateway-frontend.min.js.map', // input sourcemap from CoffeeScript compilation
+			sourceMapName: '<%= dirs.gateway.js %>/frontend/sv-wc-payment-gateway-frontend.min.map'
 		},
-		fromJs: {
-			options: {
-				sourceMap: true,
-				compress: true
-			},
-			files: [{
-				expand: true,
-				src: [ '<%= dirs.gateway.js %>/frontend/*.js', '!<%= dirs.gateway.js %>/frontend/*.min.js', '!<%= dirs.gateway.js %>/js/frontend/*.min.js' ], // uglify non-minified ES6 JS
-				dest: '<%= dirs.gateway.js %>/frontend',
-				cwd: '.',
-				rename: function (dst, src) {
-					return src.replace('.js', '.min.js');
-				}
-			}]
-		}
+		files : [{
+			src: [ '<%= dirs.gateway.js %>/js/frontend/sv-wc-payment-gateway-frontend.min.js' ], // uglify JS from CoffeeScript compilation
+			dest: '<%= dirs.gateway.js %>/frontend/sv-wc-payment-gateway-frontend.min.js'
+		}],
 	}
 
 	return config;
