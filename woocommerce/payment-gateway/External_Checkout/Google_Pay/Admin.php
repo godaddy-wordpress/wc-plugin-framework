e1f566b46467869f86cdb4103482c3dc9c9cafaf
Deiva Magalhaes <deiva@skyverge.com>
2020-10-20 14:06:35 -0700

    Update package

--- woocommerce/payment-gateway/External_Checkout/Google_Pay/Admin.php
+++ woocommerce/payment-gateway/External_Checkout/Google_Pay/Admin.php
@@ -16,7 +16,7 @@
  * versions in the future. If you wish to customize the plugin for your
  * needs please refer to http://www.skyverge.com
  *
- * @package   SkyVerge/WooCommerce/Payment-Gateway/Google-Pay
+ * @package   SkyVerge/WooCommerce/Payment-Gateway/External_Checkout/Google-Pay
  * @author    SkyVerge
  * @copyright Copyright (c) 2013-2020, SkyVerge, Inc.
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
