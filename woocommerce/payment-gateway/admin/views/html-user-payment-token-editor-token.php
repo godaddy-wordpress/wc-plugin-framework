50f3e1749684aa0bd512abb1db49964a148ce8e5
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2021-03-04 15:11:59 +0800

    Add phpdocs for variables passed to included 'template' file

--- woocommerce/payment-gateway/admin/views/html-user-payment-token-editor-token.php
+++ woocommerce/payment-gateway/admin/views/html-user-payment-token-editor-token.php
@@ -21,6 +21,15 @@
  * @copyright Copyright (c) 2013-2020, SkyVerge, Inc.
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  */
+
+/**
+ * @type string $index token index while adding tokens in AJAX requests
+ * @type string $input_name HTML name for the token fields
+ * @type array $fields token editor fields
+ * @type array $token default payment token
+ * @type string $type payment type
+ * @type array $actions payment token actions
+ */
 ?>
 
 <?php $token_input_name = $input_name . '[' . $index . ']'; ?>
