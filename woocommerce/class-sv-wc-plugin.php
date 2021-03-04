206b7da9b456205de43f48f1915cae441e554ae9
Willington Vega <wvega@wvega.com>
2021-03-03 22:49:25 -0500

    Bump version to 5.10.5

--- woocommerce/class-sv-wc-plugin.php
+++ woocommerce/class-sv-wc-plugin.php
@@ -43,7 +43,7 @@ abstract class SV_WC_Plugin {
 
 
 	/** Plugin Framework Version */
-	const VERSION = '5.10.4';
+	const VERSION = '5.10.5';
 
 	/** @var object single instance of plugin */
 	protected static $instance;
