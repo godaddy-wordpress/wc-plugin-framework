67ed9920292fc7376d6f4f34b5ca36fa43e01eea
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2021-08-27 15:51:37 +0800

    Add can_get_escaped_id_list test

--- tests/integration/HelperTest.php
+++ tests/integration/HelperTest.php
@@ -42,4 +42,31 @@ class HelperTest extends \Codeception\TestCase\WPTestCase {
 	}
 
 
+	/**
+	 * Tests that can convert an array of IDs to a comma separated list.
+	 *
+	 * @dataProvider can_get_escaped_id_list
+	 *
+	 * @param array $ids
+	 * @param string $expected
+	 */
+	public function can_get_escaped_id_list( array $ids, string $expected ) {
+
+		$this->assertSame( $expected, Framework\SV_WC_Helper::get_escaped_id_list($ids) );
+	}
+
+
+	/** @see can_get_escaped_id_list **/
+	public function provider_get_escaped_id_list() : array {
+
+		return [
+			'Non-numbers'                => [[null, false, true, 'test'], '0,0,1,1'],
+			'Integers'                   => [[1, 2, 3], '1,2,3'],
+			'Negative integers'          => [[-1, -2, -3], '1,2,3'],
+			'Numerical strings'          => [['1', '2', '3'], '1,2,3'],
+			'Negative numerical strings' => [['-1', '-2', '-3'], '1,2,3'],
+		];
+	}
+
+
 }
--- tests/unit/HelperTest.php
+++ tests/unit/HelperTest.php
@@ -14,33 +14,5 @@ class HelperTest extends \Codeception\Test\Unit {
 		require_once( 'woocommerce/class-sv-wc-helper.php' );
 	}
 
-	/**
-	 * Tests \SkyVerge\WooCommerce\PluginFramework\v5_10_8\SV_WC_Helper::get_placeholder_list()
-	 *
-	 * @param array $array values to generate list from
-	 * @param string $expected expected return value
-	 * @param string $placeholder placeholder to be used in list
-	 *
-	 * @dataProvider provider_can_get_placeholder_list
-	 */
-	public function test_can_get_placeholder_list( array $array, string $expected, string $placeholder ) {
-
-		$this->assertEquals( $expected, \SkyVerge\WooCommerce\PluginFramework\v5_10_8\SV_WC_Helper::get_placeholder_list( $array, $placeholder ) );
-	}
-
-
-	/**
-	 * Provider for test_can_get_placeholder_list()
-	 *
-	 * @return array
-	 */
-	public function provider_can_get_placeholder_list() : array {
-		return [
-			'Floating point values' => [ [1.1, 2.1], '%f, %f', '%f' ],
-			'Integer values' => [ [1, 2], '%d, %d', '%d' ],
-			'String values' => [ ['yes', 'no'], '%s, %s', '%s' ],
-		];
-	}
-
 
 }
