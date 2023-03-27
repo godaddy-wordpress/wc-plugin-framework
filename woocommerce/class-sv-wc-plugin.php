05b6a2acdef460a1afc5be3adc4c76e551652b00
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2023-03-27 13:17:42 +0800

    Version 5.10.16

--- woocommerce/class-sv-wc-plugin.php
+++ woocommerce/class-sv-wc-plugin.php
@@ -43,7 +43,7 @@ abstract class SV_WC_Plugin {
 
 
 	/** Plugin Framework Version */
-	const VERSION = '5.10.16-dev.1';
+	const VERSION = '5.10.16';
 
 	/** @var object single instance of plugin */
 	protected static $instance;
