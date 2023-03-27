9c5033a9a9b5856682c005944f32ea7808233970
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2023-03-27 13:09:10 +0800

    Bump namespaces from 5_10_15 to 5_10_16

--- tests/integration/HelperTest.php
+++ tests/integration/HelperTest.php
@@ -1,11 +1,11 @@
 <?php
 
-use SkyVerge\WooCommerce\PluginFramework\v5_10_15 as Framework;
+use SkyVerge\WooCommerce\PluginFramework\v5_10_16 as Framework;
 
 /**
  * Tests for the helper class.
  *
- * @see \SkyVerge\WooCommerce\PluginFramework\v5_10_15\SV_WC_Plugin_Compatibility
+ * @see \SkyVerge\WooCommerce\PluginFramework\v5_10_16\SV_WC_Plugin_Compatibility
  */
 class HelperTest extends \Codeception\TestCase\WPTestCase {
 
--- woocommerce/api/Abstract_Cacheable_API_Base.php
+++ woocommerce/api/Abstract_Cacheable_API_Base.php
@@ -22,14 +22,14 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  */
 
-namespace SkyVerge\WooCommerce\PluginFramework\v5_10_15\API;
+namespace SkyVerge\WooCommerce\PluginFramework\v5_10_16\API;
 
-use SkyVerge\WooCommerce\PluginFramework\v5_10_15\SV_WC_API_Base;
-use SkyVerge\WooCommerce\PluginFramework\v5_10_15\API\Traits\Cacheable_Request_Trait;
+use SkyVerge\WooCommerce\PluginFramework\v5_10_16\SV_WC_API_Base;
+use SkyVerge\WooCommerce\PluginFramework\v5_10_16\API\Traits\Cacheable_Request_Trait;
 
 defined( 'ABSPATH' ) or exit;
 
-if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_10_15\\API\\Abstract_Cacheable_API_Base' ) ) :
+if ( ! class_exists( '\\SkyVerge\\WooCommerce\\PluginFramework\\v5_10_16\\API\\Abstract_Cacheable_API_Base' ) ) :
 
 
 /**
