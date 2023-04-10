6ff65c2f92aacbaeae18de313240bec6bcd9196b
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2023-04-10 10:18:25 +0800

    Update get_order_id_for_order_edit_screen for non HPOS installs

--- woocommerce/compatibility/class-sv-wc-order-compatibility.php
+++ woocommerce/compatibility/class-sv-wc-order-compatibility.php
@@ -653,11 +653,15 @@ class SV_WC_Order_Compatibility extends SV_WC_Data_Compatibility {
 	 * @return int|null
 	 */
 	public static function get_order_id_for_order_edit_screen() : ?int {
-		global $theorder;
+		global $post, $theorder;
 
-		return $theorder instanceof \WC_Abstract_Order && ! $theorder instanceof \WC_Subscription && static::is_order_edit_screen()
-			? $theorder->get_id()
-			: null;
+		if ( SV_WC_Plugin_Compatibility::is_hpos_enabled() ) {
+			return $theorder instanceof \WC_Abstract_Order && ! $theorder instanceof \WC_Subscription && static::is_order_edit_screen()
+				? $theorder->get_id()
+				: null;
+		}
+
+		return $post->ID ?? null;
 	}
 
 
