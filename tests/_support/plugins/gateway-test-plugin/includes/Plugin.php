d5f63cf641b0650f33e5aa5ce3f31fbf40642720
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2023-05-29 18:07:17 +0800

    Version 5.11.2

--- tests/_support/plugins/gateway-test-plugin/includes/Plugin.php
+++ tests/_support/plugins/gateway-test-plugin/includes/Plugin.php
@@ -1,7 +1,7 @@
 <?php
 namespace SkyVerge\WooCommerce\GatewayTestPlugin;
 
-use SkyVerge\WooCommerce\PluginFramework\v5_11_1 as Framework;
+use SkyVerge\WooCommerce\PluginFramework\v5_11_2 as Framework;
 
 defined( 'ABSPATH' ) or exit;
 
