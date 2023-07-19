685565c4cd51a7de62fe73fae0434a5f954909b1
ragulka <illimar.tambek@gmail.com>
2023-07-19 13:47:49 +0300

    bump FW version in plugin & sample

--- woocommerce/class-sv-wc-plugin.php
+++ woocommerce/class-sv-wc-plugin.php
@@ -43,7 +43,7 @@ abstract class SV_WC_Plugin {
 
 
 	/** Plugin Framework Version */
-	const VERSION = '5.11.5';
+	const VERSION = '5.11.6';
 
 	/** @var object single instance of plugin */
 	protected static $instance;
