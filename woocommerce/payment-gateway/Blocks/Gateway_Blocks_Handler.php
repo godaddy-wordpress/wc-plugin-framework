5b8f5e3cb0ce74b2af60ec908c30bb56f9f5da19
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2023-09-21 12:53:02 +0800

    Delay loading of gateways in blocks handler

--- woocommerce/payment-gateway/Blocks/Gateway_Blocks_Handler.php
+++ woocommerce/payment-gateway/Blocks/Gateway_Blocks_Handler.php
@@ -5,7 +5,9 @@ namespace SkyVerge\WooCommerce\PluginFramework\v5_11_8\Payment_Gateway\Blocks;
 use Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface;
 use Automattic\WooCommerce\Blocks\Payments\PaymentMethodRegistry;
 use SkyVerge\WooCommerce\PluginFramework\v5_11_8\Blocks\Blocks_Handler;
+use SkyVerge\WooCommerce\PluginFramework\v5_11_8\SV_WC_Payment_Gateway;
 use SkyVerge\WooCommerce\PluginFramework\v5_11_8\SV_WC_Payment_Gateway_Plugin;
+
 
 use function Patchwork\Redefinitions\LanguageConstructs\_require_once;
 
@@ -24,12 +26,6 @@ class Gateway_Blocks_Handler extends Blocks_Handler
 {
 
 
-	/** @var IntegrationInterface[] */
-	protected array $checkout_block_integrations = [];
-
-	/** @var IntegrationInterface[] */
-	protected array $cart_block_integrations = [];
-
 	/**
 	 * Gateway blocks handler constructor.
 	 *
@@ -40,18 +36,6 @@ class Gateway_Blocks_Handler extends Blocks_Handler
 	public function __construct(SV_WC_Payment_Gateway_Plugin $plugin) {
 
 		parent::__construct($plugin);
-
-		require_once( $this->plugin->get_framework_path() . '/payment-gateway/Blocks/Gateway_Checkout_Block_Integration.php' );
-
-		foreach ( $plugin->get_gateways() as $gateway ) {
-			if ( $checkout = $gateway->get_checkout_block_integration_instance() ) {
-				$this->checkout_block_integrations[ $gateway->get_id_dasherized() ] = $checkout;
-			}
-
-			if ( $cart = $gateway->get_cart_block_integration_instance() ) {
-				$this->cart_block_integrations[ $gateway->get_id_dasherized() ] = $cart;
-			}
-		}
 	}
 
 
@@ -72,14 +56,20 @@ class Gateway_Blocks_Handler extends Blocks_Handler
 
 		if ( $this->is_checkout_block_compatible() ) {
 
-			foreach ( $this->checkout_block_integrations as $checkout_integration ) {
+			require_once( $this->plugin->get_framework_path() . '/payment-gateway/Blocks/Gateway_Checkout_Block_Integration.php' );
 
-				add_action(
-					'woocommerce_blocks_payment_method_type_registration',
-					function( PaymentMethodRegistry $payment_method_registry ) use ( $checkout_integration ) {
-						$payment_method_registry->register( $checkout_integration );
-					}
-				);
+			/** @var SV_WC_Payment_Gateway $gateway */
+			foreach ( $this->plugin->get_gateways() as $gateway ) {
+
+				if ( $checkout_integration = $gateway->get_checkout_block_integration_instance() ) {
+
+					add_action(
+						'woocommerce_blocks_payment_method_type_registration',
+						function ( PaymentMethodRegistry $payment_method_registry ) use ( $checkout_integration ) {
+							$payment_method_registry->register($checkout_integration);
+						}
+					);
+				}
 			}
 		}
 	}
