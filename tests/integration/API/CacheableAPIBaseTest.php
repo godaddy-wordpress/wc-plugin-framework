a1ba640d561fa036b6f664752acc25fac07acbd9
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2023-12-14 10:06:11 +0900

    Update namespace refs

--- tests/integration/API/CacheableAPIBaseTest.php
+++ tests/integration/API/CacheableAPIBaseTest.php
@@ -1,10 +1,10 @@
 <?php
 
-use SkyVerge\WooCommerce\PluginFramework\v5_11_12 as Framework;
-use SkyVerge\WooCommerce\PluginFramework\v5_11_12\API\Abstract_Cacheable_API_Base;
-use SkyVerge\WooCommerce\PluginFramework\v5_11_12\API\Traits\Cacheable_Request_Trait;
-use SkyVerge\WooCommerce\PluginFramework\v5_11_12\SV_WC_API_JSON_Request;
-use SkyVerge\WooCommerce\PluginFramework\v5_11_12\SV_WC_API_Request;
+use SkyVerge\WooCommerce\PluginFramework\v5_12_0 as Framework;
+use SkyVerge\WooCommerce\PluginFramework\v5_12_0\API\Abstract_Cacheable_API_Base;
+use SkyVerge\WooCommerce\PluginFramework\v5_12_0\API\Traits\Cacheable_Request_Trait;
+use SkyVerge\WooCommerce\PluginFramework\v5_12_0\SV_WC_API_JSON_Request;
+use SkyVerge\WooCommerce\PluginFramework\v5_12_0\SV_WC_API_Request;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', true );
