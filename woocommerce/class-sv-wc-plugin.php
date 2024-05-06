bcbff341bacb76777e385b7072897d8b1c5a9328
Illimar Tambek <itambek@godaddy.com>
2024-05-06 09:20:40 +0300

    bump version in plugin & lock file

--- woocommerce/class-sv-wc-plugin.php
+++ woocommerce/class-sv-wc-plugin.php
@@ -47,7 +47,7 @@ abstract class SV_WC_Plugin {
 
 
 	/** Plugin Framework Version */
-	const VERSION = '5.12.2';
+	const VERSION = '5.12.3';
 
 	/** @var object single instance of plugin */
 	protected static $instance;
