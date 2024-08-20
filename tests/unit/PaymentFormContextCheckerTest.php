713170c6b0157cd526ad466906d1b5d137bc8453
Nabeel Molham <nmolham@godaddy.com>
2024-08-20 10:22:16 +1000

    Unit test PaymentFormContextChecker::maybeSetContext method

--- tests/unit/PaymentFormContextCheckerTest.php
+++ tests/unit/PaymentFormContextCheckerTest.php
@@ -6,6 +6,8 @@ use Mockery;
 use ReflectionException;
 use SkyVerge\WooCommerce\PluginFramework\v5_13_0\PaymentFormContextChecker;
 use SkyVerge\WooCommerce\PluginFramework\v5_13_0\Tests\TestCase;
+use WooCommerce;
+use WP_Mock;
 
 class PaymentFormContextCheckerTest extends TestCase
 {
@@ -35,5 +37,34 @@ class PaymentFormContextCheckerTest extends TestCase
 			'wc_TEST_GATEWAY_ID_payment_form_context',
 			$this->invokeInaccessibleMethod($this->testObject, 'getContextSessionKeyName')
 		);
+	}
+
+	/**
+	 * @covers \SkyVerge\WooCommerce\PluginFramework\v5_13_0\PaymentFormContextChecker::maybeSetContext()
+	 */
+	public function testCanSetContext() : void
+	{
+		$this->testObject->expects('getCurrentPaymentFormContext')
+			->once()
+			->andReturn('TEST_FORM_CONTEXT');
+
+		$this->testObject->expects('getContextSessionKeyName')
+			->once()
+			->andReturn('TEST_CONTEXT_SESSION_KEY_NAME');
+
+		WP_Mock::userFunction('WC')
+			->once()
+			->andReturn($wooCommerce = Mockery::mock(WooCommerce::class));
+
+		$session = Mockery::mock('WC_Session');
+		$session->expects('set')
+			->once()
+			->with('TEST_CONTEXT_SESSION_KEY_NAME', 'TEST_FORM_CONTEXT');
+
+		$wooCommerce->session = $session;
+
+		$this->testObject->maybeSetContext();
+
+		$this->assertConditionsMet();
 	}
 }
