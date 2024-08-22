98c173658aaa5a29262d18e6c2c363a4999f93c0
Nabeel Molham <nmolham@godaddy.com>
2024-08-22 15:11:39 +1000

    Fix namespace mapping

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,13 +5,6 @@ const ABSPATH = 'foo/bar';
 define('PLUGIN_ROOT_DIR', dirname(__DIR__));
 
 require_once PLUGIN_ROOT_DIR.'/vendor/autoload.php';
-require_once PLUGIN_ROOT_DIR.'/woocommerce/class-sv-wc-plugin.php';
-require_once PLUGIN_ROOT_DIR.'/woocommerce/class-sv-wc-plugin-exception.php';
-require_once PLUGIN_ROOT_DIR.'/woocommerce/Enums/Traits/EnumTrait.php';
-require_once PLUGIN_ROOT_DIR.'/woocommerce/Enums/PaymentFormContext.php';
-require_once PLUGIN_ROOT_DIR.'/woocommerce/Traits/IsSingletonTrait.php';
 
 WP_Mock::setUsePatchwork(true);
 WP_Mock::bootstrap();
-
-require_once PLUGIN_ROOT_DIR.'/woocommerce/class-sv-wc-helper.php';
