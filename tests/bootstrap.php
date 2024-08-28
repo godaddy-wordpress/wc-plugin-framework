9817b0a8da029ed4ef4af11f7db41842b6414a9e
Nabeel Molham <nmolham@godaddy.com>
2024-08-28 16:01:19 +1000

    Setup class mapping via composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,6 @@ const ABSPATH = 'foo/bar';
 define('PLUGIN_ROOT_DIR', dirname(__DIR__));
 
 require_once PLUGIN_ROOT_DIR.'/vendor/autoload.php';
-require_once PLUGIN_ROOT_DIR.'/woocommerce/class-sv-wc-plugin.php';
-require_once PLUGIN_ROOT_DIR.'/woocommerce/class-sv-wc-plugin-exception.php';
 
 WP_Mock::setUsePatchwork(true);
 WP_Mock::bootstrap();
-
-require_once PLUGIN_ROOT_DIR.'/woocommerce/class-sv-wc-helper.php';
