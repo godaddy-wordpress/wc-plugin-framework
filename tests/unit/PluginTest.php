4ed4e05303cb1022767639f3da4c377b5e8d7081
Nabeel Molham <nmolham@godaddy.com>
2024-08-29 18:11:55 +1000

    Unit test SV_WC_Plugin::assert method

--- tests/unit/PluginTest.php
+++ tests/unit/PluginTest.php
@@ -47,4 +47,42 @@ class PluginTest extends TestCase
 			$this->invokeInaccessibleMethod($this->testObject, 'logger')
 		);
 	}
+
+	/**
+	 * @covers \SkyVerge\WooCommerce\PluginFramework\v5_14_0\SV_WC_Plugin::assert()
+	 * @throws ReflectionException
+	 */
+	public function testCanAssert() : void
+	{
+		WP_Mock::userFunction('wp_debug_backtrace_summary')->never();
+
+		$this->testObject->expects('logger')->never();
+
+		$this->assertNull(
+			$this->invokeInaccessibleMethod($this->testObject, 'assert', true)
+		);
+	}
+
+	/**
+	 * @covers \SkyVerge\WooCommerce\PluginFramework\v5_14_0\SV_WC_Plugin::assert()
+	 * @throws ReflectionException
+	 */
+	public function testCanCatchFailedAssertion() : void
+	{
+		WP_Mock::userFunction('wp_debug_backtrace_summary')
+			->once()
+			->andReturn('TEST_DEBUG_BACKTRACE_SUMMARY');
+
+		$this->testObject->expects('logger')
+			->once()
+			->andReturn($logger = Mockery::mock('WC_Logger_Interface'));
+
+		$logger->expects('debug')
+			->once()
+			->with('Assertion failed, backtrace summery: TEST_DEBUG_BACKTRACE_SUMMARY');
+
+		$this->assertNull(
+			$this->invokeInaccessibleMethod($this->testObject, 'assert', false)
+		);
+	}
 }
