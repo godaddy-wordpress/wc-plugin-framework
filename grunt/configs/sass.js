56f78d291e4cffed0a6761c32cb710d00b1c7b3e
Nabeel Molham <nmolham@godaddy.com>
2024-09-04 14:25:54 +1000

    Simplify returned configs

--- grunt/configs/sass.js
+++ grunt/configs/sass.js
@@ -2,40 +2,38 @@
 module.exports = function () {
 	'use strict';
 
-	let config = {};
-
 	// Compile all .scss files.
-	config.sass = {
-		compile: {
-			options: {
-				'style': 'compressed',
-				'source-map': true
-			},
-			files: [
-				{
-					expand: true,
-					cwd: '<%= dirs.general.css %>/admin/',
-					dest: '<%= dirs.general.css %>/admin/',
-					src: ['*.scss', '!_*.scss'],
-					ext: '.min.css'
+	return {
+		sass: {
+			compile: {
+				options: {
+					'style': 'compressed',
+					'source-map': true
 				},
-				{
-					expand: true,
-					cwd: '<%= dirs.gateway.css %>/admin/',
-					dest: '<%= dirs.gateway.css %>/admin/',
-					src: ['*.scss', '!_*.scss'],
-					ext: '.min.css'
-				},
-				{
-					expand: true,
-					cwd: '<%= dirs.gateway.css %>/frontend/',
-					dest: '<%= dirs.gateway.css %>/frontend/',
-					src: ['*.scss', '!_*.scss'],
-					ext: '.min.css'
-				}
-			]
+				files: [
+					{
+						expand: true,
+						cwd: '<%= dirs.general.css %>/admin/',
+						dest: '<%= dirs.general.css %>/admin/',
+						src: ['*.scss', '!_*.scss'],
+						ext: '.min.css'
+					},
+					{
+						expand: true,
+						cwd: '<%= dirs.gateway.css %>/admin/',
+						dest: '<%= dirs.gateway.css %>/admin/',
+						src: ['*.scss', '!_*.scss'],
+						ext: '.min.css'
+					},
+					{
+						expand: true,
+						cwd: '<%= dirs.gateway.css %>/frontend/',
+						dest: '<%= dirs.gateway.css %>/frontend/',
+						src: ['*.scss', '!_*.scss'],
+						ext: '.min.css'
+					}
+				]
+			}
 		}
 	};
-
-	return config;
 };
