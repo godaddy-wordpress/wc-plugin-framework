c9a8c187405abb525b7d4aa81463e3e883235d65
Ashley Gibson <agibson@godaddy.com>
2024-11-25 14:05:47 +0000

    Release: v5.15.2

--- tests/integration/payment-gateway/GatewayPluginTest.php
+++ tests/integration/payment-gateway/GatewayPluginTest.php
@@ -3,7 +3,7 @@
 /**
  * Tests for the gateway plugin class.
  *
- * @see \SkyVerge\WooCommerce\PluginFramework\v5_15_1\SV_WC_Payment_Gateway_Plugin
+ * @see \SkyVerge\WooCommerce\PluginFramework\v5_15_2\SV_WC_Payment_Gateway_Plugin
  */
 class GatewayPluginTest extends \Codeception\TestCase\WPTestCase {
 
