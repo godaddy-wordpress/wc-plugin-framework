ef20a36d8d17d58ca5c39eca93e23a6e4bcb7a2c
Ashley Gibson <agibson@godaddy.com>
2025-03-03 13:54:48 +0000

    Create preview values trait

---
+++ woocommerce/Emails/Traits/HasEmailPreviewValuesTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SkyVerge\WooCommerce\PluginFramework\v5_15_5\Emails\Traits;
+
+/**
+ * Adds improved support for the experimental "Email Improvements" feature, by populating any missing properties
+ * with default/placeholder values.
+ *
+ * {@see \Automattic\WooCommerce\Internal\Admin\EmailPreview\EmailPreview::set_email_type()}
+ */
+trait HasEmailPreviewValuesTrait
+{
+	/**
+	 * Sets "Preview" values on the email object.
+	 */
+	abstract public function setPreviewValues() : void;
+}
