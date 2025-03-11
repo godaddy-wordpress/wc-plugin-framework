275466c6ebd397c293b6a9f525f24ca7c314c876
Ashley Gibson <99189195+agibson-godaddy@users.noreply.github.com>
2025-03-11 14:53:28 +0000

    Add additional helper methods to the trait `HasEmailPreviewValuesTrait` (#753)
    * Add additional helper methods to the trait
    
    * Update changelog
    
    * Pass in null
    
    * Minor inline docs tweaks.
    
    ---------
    
    Co-authored-by: Drew Jaynes <113622064+ajaynes-godaddy@users.noreply.github.com>

--- woocommerce/Emails/Traits/HasEmailPreviewValuesTrait.php
+++ woocommerce/Emails/Traits/HasEmailPreviewValuesTrait.php
@@ -2,6 +2,9 @@
 
 namespace SkyVerge\WooCommerce\PluginFramework\v5_15_6\Emails\Traits;
 
+use Automattic\WooCommerce\Internal\Admin\EmailPreview\EmailPreview;
+use WC_Product;
+
 /**
  * Adds improved support for the experimental "Email Improvements" feature, by populating any missing properties
  * with default/placeholder values.
@@ -14,4 +17,41 @@ trait HasEmailPreviewValuesTrait
 	 * Sets "Preview" values on the email object.
 	 */
 	abstract public function setPreviewValues() : void;
+
+	/**
+	 * Gets a random, real product from the site.
+	 *
+	 * For use in rendering examples for email templates.
+	 * @return WC_Product|null
+	 */
+	protected function getRandomProduct() : ?WC_Product
+	{
+		$products = array_values(wc_get_products([
+			'post_type'      => 'product',
+			'posts_per_page' => 1,
+			'orderby'        => 'rand',
+		]));
+
+		if (isset($products[0]) && $products[0] instanceof WC_Product) {
+			return $products[0];
+		}
+
+		return null;
+	}
+
+	/**
+	 * Gets a dummy (fake) product.
+	 *
+	 * For use in rendering examples for email templates.
+	 * @return WC_Product
+	 */
+	protected function getDummyProduct() : WC_Product
+	{
+		if (class_exists(EmailPreview::class) && method_exists(EmailPreview::class, 'get_dummy_product_when_not_set')) {
+			return EmailPreview::instance()->get_dummy_product_when_not_set(null);
+		} else {
+			// we should really never end up here!
+			return new WC_Product();
+		}
+	}
 }
