fb5f053ad3e9630d15345d4e8f4bb53988832086
Ashley Gibson <agibson@godaddy.com>
2026-03-05 12:54:46 +0000

    Add JsonSerializable

---
+++ woocommerce/Abilities/Contracts/JsonSerializable.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SkyVerge\WooCommerce\PluginFramework\v6_0_1\Abilities\Contracts;
+
+/**
+ * Contract for objects that provide both JSON serialization and a JSON Schema describing their output.
+ *
+ * Extends PHP's native {@see \JsonSerializable} with a static method that returns
+ * the JSON Schema for the shape produced by {@see \JsonSerializable::jsonSerialize()}.
+ * This allows abilities to reference the schema directly from the serializable object
+ * rather than duplicating it in each ability definition.
+ *
+ * @since 6.1.0
+ */
+interface JsonSerializable extends \JsonSerializable
+{
+	/**
+	 * Returns the JSON Schema describing the shape of {@see \JsonSerializable::jsonSerialize()} output.
+	 *
+	 * @since 6.1.0
+	 *
+	 * @return array JSON Schema array (e.g. with 'type', 'properties', etc.)
+	 */
+	public static function getJsonSchema() : array;
+}
